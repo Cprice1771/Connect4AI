0b1ded2e35846dbbfa76d6c92c914f00515043a2
Cprice <thrashers231@yahoo.com>
2015-03-30 00:05:46 -0400

    -Added console app stuff and written report

--- ConnectRConsole/Program.cs
+++ ConnectRConsole/Program.cs
@@ -11,5 +11,42 @@ namespace ConnectRConsole
         static void Main(string[] args)
         {
         }
+
+        public static void PrintBoard(List<List<SquareSimple>> board)
+        {
+            for (int i = 0; i < board[0].Count; i++)
+                Console.Write("\t" + i);
+
+            Console.WriteLine();
+
+            for (int i = 0; i < board.Count; i++)
+            {
+                Console.Write(i + "\t");
+                for (int j = 0; j < board[i].Count; j++)
+                {
+                    string play = "";
+                    switch (board[i][j].State)
+                    {
+                        case SquareState.Black:
+                            play = "X";
+                            break;
+                        case SquareState.Red:
+                            play = "O";
+                            break;
+                        default:
+                            play = "-";
+                            break;
+                    }
+                    Console.Write(play + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < board[0].Count; i++)
+                Console.Write("\t" + i);
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
     }
 }
---
+++ ConnectRConsole/SquareSimple.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectRConsole
+{
+    public class SquareSimple
+    {
+        public SquareState State { get; set; }
+
+        public SquareSimple()
+        {
+            State = SquareState.Blank;
+        }
+
+        public SquareSimple Clone()
+        {
+            SquareSimple ss = new SquareSimple();
+            switch (State)
+            {
+                case SquareState.Black:
+                    ss.State = SquareState.Black;
+                    break;
+                case SquareState.Red:
+                    ss.State = SquareState.Red;
+                    break;
+                default:
+                    ss.State = SquareState.Blank;
+                    break;
+            }
+            return ss;
+        }
+    }
+}
