e98c04664190fa5e8bcaa40b9c798ef13c01f2eb
Cprice <thrashers231@yahoo.com>
2015-04-01 09:15:12 -0400

    -Added console stuff

--- ConnectRConsole/Program.cs
+++ ConnectRConsole/Program.cs
@@ -8,8 +8,80 @@ namespace ConnectRConsole
 {
     class Program
     {
+        private static List<List<SquareSimple>> _boardList;
+        private static Turn _turn;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("How many Rows?");
+
+            int rows = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many Columns?");
+
+            int cols = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Who goes first?");
+
+            string firstTurn = Console.ReadLine();
+            
+            if(firstTurn.Contains("Red"))
+                _turn = Turn.Red;
+            else
+                _turn = Turn.Black;
+                
+            for (int i = 0; i < rows; i++)
+            {
+                _boardList.Add(new List<SquareSimple>());
+                for (int j = 0; j < cols; j++)
+                {
+                    SquareSimple s = new SquareSimple();
+                    s.State = SquareState.Blank;
+                    _boardList[i].Add(s);
+                }
+            }
+
+            PrintBoard(_boardList);
+
+            while (true)
+            {
+                Console.WriteLine("What Coloumn do you wish to play in?");
+                int play = int.Parse(Console.ReadLine());
+
+                MakePlay(play);
+
+                PrintBoard(_boardList);
+            }
+
+
+        }
+
+        private static bool MakePlay(int col)
+        {
+            SquareSimple square = null;
+            for (int i = 0; i < _boardList.Count; i++)
+            {
+                if (_boardList[i][col].State == SquareState.Blank)
+                    square = _boardList[i][col];
+
+            }
+
+            //If the row is full or the square given was invalid, do nothing
+            if (square == null || square.State != SquareState.Blank)
+                return false;
+
+            if (_turn == Turn.Red)
+            {
+                square.State = SquareState.Red;
+                _turn = Turn.Black;
+            }
+            else
+            {
+                square.State = SquareState.Black;
+                _turn = Turn.Red;
+            }
+            return true;
+
         }
 
         public static void PrintBoard(List<List<SquareSimple>> board)
